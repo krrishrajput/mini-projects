c7fc675584fac186071a9e5b5fd7c2a93407f292
Krish Rajput <krishrajput1776cosmic@gmail.com>
2026-08-06 10:19:36 +0530

    fixed minor issue

--- file_86.py
+++ file_86.py
@@ -5,3 +5,17 @@ def factorial(n):
         return 1
     return n * factorial(n-1)
 
+
+# 2026-08-06 10:19:35.956173
+def binary_search(arr, target):
+    low, high = 0, len(arr)-1
+    while low <= high:
+        mid = (low+high)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid+1
+        else:
+            high = mid-1
+    return -1
+
