dcfcc153c2c59f89796e1cb98d659d5c307e395d
Krish Rajput <krishrajput1776cosmic@gmail.com>
2026-08-21 00:53:57 +0530

    improved existing logic

--- file_86.py
+++ file_86.py
@@ -19,3 +19,17 @@ def binary_search(arr, target):
             high = mid-1
     return -1
 
+
+# 2026-08-21 00:53:57.906563
+def binary_search(arr, target):
+    low, high = 0, len(arr)-1
+    while low <= high:
+        mid = (low+high)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid+1
+        else:
+            high = mid-1
+    return -1
+
